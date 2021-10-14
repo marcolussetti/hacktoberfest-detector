4dd3bb8e18b7535a57ed6ab692e92436c73dc80a
marcolussetti <git@marcolussetti.com>
2021-10-13 22:42:56 -0700

    Make the notification prettier

--- optindetector.js
+++ optindetector.js
@@ -7,11 +7,21 @@ function isHacktoberfestEnabled() {
 }
 
 function enableLabel() {
-    document.body.style.border = "5px solid green"
+    if (!document.querySelector("#hacktoberfest-optin-label")) {
+        var labelElement = document.createElement("div")
+        labelElement.id = "hacktoberfest-optin-label"
+        labelElement.innerHTML = '<h5 class="mb-1"><img style="vertical-align:middle" src="https://avatars.githubusercontent.com/u/35706162?s=35"> &nbsp;This repository has opted-in to the Hacktoberfest project!</h5>'
+        labelElement.className = "flex-justify-between flex-column flex-md-row flex-md-items-center flash flash-success mb-3 d-flex"
+        document.querySelector("#repo-content-pjax-container .gutter-condensed div").prepend(labelElement)
+    }
 }
 
 function disableLabel() {
     document.body.style.border = "0px"
+    let labelElement = document.querySelector("#hacktoberfest-optin-label")
+    if (labelElement) {
+        labelElement.parentElement.removeChild(labelElement)
+    }
 }
 
 function getProject() {
