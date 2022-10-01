a18f6a40a983ec245829042d1fe0d98a26758d28
marcolussetti <git@marcolussetti.com>
2022-10-01 09:45:32 -0700

    improve chromium build scripts

--- build-chromium.sh
+++ build-chromium.sh
@@ -6,4 +6,7 @@ mkdir chromium
 cp -r `ls -A | grep -v "^chromium" | grep -v "web-ext"` chromium/
 mv chromium/manifest-chromium.json chromium/manifest.json
 cd chromium
-web-ext run -t chromium
+web-ext build
+for f in web-ext-artifacts/*.zip; do
+	cp "$f" "../web-ext-artifacts/$(basename "$f" .zip)-chromium.zip"
+done
